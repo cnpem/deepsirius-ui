17a38af9849af3237b5d0ead0b7163be63847c9a
Matheus L. Bernardi <matheus.bernardi@lnls.br>
2023-04-27 10:43:31 -0300

    extract useTree from fs-treeview and import it from src/hooks
    Signed-off-by: Matheus L. Bernardi <matheus.bernardi@lnls.br>

--- src/components/fs-treeview.tsx
+++ src/components/fs-treeview.tsx
@@ -13,19 +13,9 @@ import { Label } from "~/components/ui/label";
 import { type NodeRendererProps, Tree, type NodeApi } from "react-arborist";
 import { File, Folder, FolderOpen } from "lucide-react";
 import { useState } from "react";
-import useSWR from "swr";
+import { type nodeItem } from "~/types/nodeItem";
+import { useTree, treeFetcher } from "~/hooks/tree";
 import { env } from "~/env.mjs";
-
-type nodeItem = {
-  name: string;
-  path: string;
-  relativePath: string;
-  type: string;
-  isSymbolicLink: boolean;
-  sizeInBytes: number;
-  size: string;
-  children?: nodeItem[];
-};
 
 function Node({ node, style, dragHandle }: NodeRendererProps<nodeItem>) {
   const Icon =
@@ -48,72 +38,15 @@ function Node({ node, style, dragHandle }: NodeRendererProps<nodeItem>) {
   );
 }
 
-type fetchParams = {
-  path: string;
-  url: string;
-};
-async function fetcher(props: fetchParams): Promise<nodeItem[]> {
-  const { url, path } = props;
-  const params = { path: path };
-
-  try {
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(params),
-    });
-
-    if (res.ok) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const item: nodeItem = await res.json();
-      return [item];
-    } else {
-      throw new Error("Network response was not ok");
-    }
-  } catch (err) {
-    throw err;
-  }
-}
-
-function useTree(path: string) {
-  const { data, isLoading, mutate } = useSWR<nodeItem[]>(
-    { url: "/api/filesystem", path: path },
-    fetcher,
-    {
-      fallbackData: new Array<nodeItem>(),
-      keepPreviousData: true,
-      revalidateOnFocus: false,
-    }
-  );
-
-  return {
-    tree: data,
-    isLoading,
-    mutate,
-  };
-}
-
 export function FsTreeView() {
   const treePath = env.NEXT_PUBLIC_TREE_PATH;
   const [path, setPath] = useState(treePath);
   const [open, setOpen] = useState(false);
   const { tree, mutate } = useTree(treePath);
-  // const [tree, setTree] = useState(new Array<nodeItem>());
-
-  // useEffect(()=>{
-  //   async function fetchTree(){
-  //     const arvore = await fetcher({url:"/api/filesystem",path:home});
-  //     setTree(arvore);
-  //   }
-  //   void fetchTree();
-  // },[])
 
   const handleReplaceNode = (nodePath: string, newNode: nodeItem) => {
     if (tree && tree[0]) {
       const updatedTree: nodeItem = replaceNode(tree[0], nodePath, newNode);
-      // setTree([updatedTree]);
       void mutate([updatedTree], false);
     }
   };
@@ -141,7 +74,7 @@ export function FsTreeView() {
   const handleActivate = (node: NodeApi<nodeItem>) => {
     setPath(node.data.path);
     if (node.isOpen && node.data.type === "directory") {
-      fetcher({ url: "/api/filesystem", path: node.data.path })
+      treeFetcher({ url: "/api/filesystem", path: node.data.path })
         .then((updatedNode) => {
           if (updatedNode && updatedNode[0]) {
             handleReplaceNode(node.data.path, updatedNode[0]);
@@ -154,7 +87,11 @@ export function FsTreeView() {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        {tree && <Button onClick={()=>setPath(treePath)} variant="outline">...</Button>}
+        {tree && (
+          <Button onClick={() => setPath(treePath)} variant="outline">
+            ...
+          </Button>
+        )}
       </DialogTrigger>
       <DialogContent className="sm:max-w-[825px]">
         <DialogHeader>
--- src/server/fs-tree.ts
+++ src/server/fs-tree.ts
@@ -3,35 +3,21 @@ import { type ScanOptions } from "dree";
 
 const options: ScanOptions = {
   depth: 3,
-  hash:false,
+  hash: false,
   exclude: ["node_modules", "__pycache__"],
   excludeEmptyDirectories: true,
-  symbolicLinks:false,
+  symbolicLinks: false,
   showHidden: false,
-  stat:false,
+  stat: false,
 };
 
-// export function getChildren(path: string) {
-//   const directoryPath = path ? path : "/";
-//   return new Promise<dree.Dree>((resolve, reject) => {
-//     dree
-//       .scanAsync(directoryPath, options)
-//       .then((tree) => {
-//         // console.log(tree.stat);
-//         resolve(tree);
-//       })
-//       .catch((error) => reject(error));
-//   });
-// }
 export async function getChildren(path: string): Promise<dree.Dree> {
   const directoryPath = path ? path : "/";
   try {
     const tree = await dree.scanAsync(directoryPath, options);
     // console.log(tree);
-    // console.log(tree.stat)
     return tree;
   } catch (error) {
     throw error;
   }
 }
-

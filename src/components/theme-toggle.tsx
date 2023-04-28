d1b86c175b4ed250a3a15a917d889dfa52102aa3
Matheus L. Bernardi <matheus.bernardi@lnls.br>
2023-04-28 15:26:42 -0300

    lil refactor in theme toggle
    Signed-off-by: Matheus L. Bernardi <matheus.bernardi@lnls.br>

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -8,10 +8,11 @@ export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <Button variant="ghost" size="sm" onClick={() => theme === 'light' ? setTheme('dark') : setTheme('light')}>
+    <Button variant="ghost" size="sm" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
       <Icons.sun className="rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100" />
       <Icons.moon className="absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
 }
+// git commit 

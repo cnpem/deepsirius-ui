e6e8e3c47532b5bf730797c672b3b25ad563fa79
Matheus L. Bernardi <matheus.bernardi@lnls.br>
2023-05-22 18:06:18 -0300

    changed tabwidth back to 2
    Signed-off-by: Matheus L. Bernardi <matheus.bernardi@lnls.br>

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,14 +1,14 @@
 /** @type {import('@trivago/prettier-plugin-sort-imports').PrettierConfig}*/
 const config = {
-    printWidth: 80,
-    tabWidth: 4,
-    trailingComma: 'all',
-    singleQuote: true,
-    semi: true,
-    importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true,
-    plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
 };
 
 module.exports = config;

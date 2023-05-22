310c36b4afc9b30b8028642a65a83ad8002278a2
Matheus L. Bernardi <matheus.bernardi@lnls.br>
2023-05-22 18:01:12 -0300

    added import sorter on formatter
    Signed-off-by: Matheus L. Bernardi <matheus.bernardi@lnls.br>

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,14 @@
-/** @type {import("prettier").Config} */
+/** @type {import('@trivago/prettier-plugin-sort-imports').PrettierConfig}*/
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    printWidth: 80,
+    tabWidth: 4,
+    trailingComma: 'all',
+    singleQuote: true,
+    semi: true,
+    importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    plugins: [require.resolve('prettier-plugin-tailwindcss')],
 };
 
 module.exports = config;

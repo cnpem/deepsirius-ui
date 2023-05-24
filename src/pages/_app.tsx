c90540e485f4865fa8219856a48808386ae2e8c4
Matheus L. Bernardi <matheus.bernardi@lnls.br>
2023-05-24 08:59:18 -0300

    setup trpc for api endpoints instead of /api/something
    Signed-off-by: Matheus L. Bernardi <matheus.bernardi@lnls.br>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { ThemeProvider } from "next-themes";
-import { SessionProvider } from "next-auth/react";
-
-import "~/styles/globals.css";
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider } from 'next-themes';
+import { type AppType } from 'next/app';
+import '~/styles/globals.css';
+import { api } from '~/utils/api';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -18,4 +18,4 @@ const MyApp: AppType<{ session: Session | null }> = ({
   );
 };
 
-export default MyApp;
+export default api.withTRPC(MyApp);

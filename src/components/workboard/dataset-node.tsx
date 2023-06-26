b540bd6ef25c63e0bd734d4e56dfa4ab6d17687e
Bruno Carlos <bruno.carlos@lnls.br>
2023-06-26 10:46:59 -0300

    removing unused wrapper from common-node-utils  and temporary fix to the dataset-node

--- src/components/workboard/dataset-node.tsx
+++ src/components/workboard/dataset-node.tsx
@@ -1,36 +1,18 @@
-import {
-  useNodeId,
-  type Node,
-  Position,
-  Handle,
-  type NodeProps,
-} from "reactflow";
-import { NodeData, NodeWrapper } from "./common-node-utils";
+import { Handle, Position } from 'reactflow';
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "~/components/ui/accordion";
-import { Input } from "~/components/ui/input";
+} from '~/components/ui/accordion';
+import { Input } from '~/components/ui/input';
 
-type DatasetNode = Node<NodeData>;
-export function DatasetNode({ data }: NodeProps<NodeData>) {
-  const { label = "dataset", status = "inactive" } = data;
-  const nodeId = useNodeId() || "";
-
-  // const handleConnection = (c: Connection) => {
-  //   console.log({c});
-  //   console.log("handling connection in to " + { label } + {nodeId});
-  //   const sourcenode = nodes.find((node: Node) => node.id === c.source);
-  //   console.log(sourcenode?.data);
-  // };
-
+export function DatasetNode() {
   return (
-    <NodeWrapper label={label + nodeId} status={status}>
+    <div>
       <Handle type="target" position={Position.Left} />
       <div className="flex h-full flex-col items-center justify-center">
-        <div>{`I'm the ${label} ${nodeId}`}</div>
+        <div>{`I'm the old dataset`}</div>
       </div>
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1">
@@ -44,6 +26,6 @@ export function DatasetNode({ data }: NodeProps<NodeData>) {
         </AccordionItem>
       </Accordion>
       <Handle type="source" position={Position.Right} />
-    </NodeWrapper>
+    </div>
   );
 }

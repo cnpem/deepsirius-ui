85747a799ec50274d86341b05ec04c672d31989b
Matheus Luís <matheus.bernardi@lnls.br>
2024-01-18 15:30:19 -0300

    refactoring: workboard layout

--- src/components/avatar-dropdown.tsx
+++ src/components/avatar-dropdown.tsx
@@ -1,6 +1,10 @@
+import { ArrowLeftIcon } from 'lucide-react';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useCallback } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
+import { toast } from 'sonner';
 import { Icons } from '~/components/icons';
 import { Button } from '~/components/ui/button';
 import {
@@ -12,23 +16,22 @@ import {
   DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from '~/components/ui/dropdown-menu';
-import { useStore, useStoreActions } from '~/hooks/use-store';
+import { useStoreActions } from '~/hooks/use-store';
 
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 export function AvatarDrop() {
   const { data: sessionData } = useSession();
+  const router = useRouter();
 
-  const { workspacePath } = useStore();
   const { resetStore } = useStoreActions();
 
   const logOut = useCallback(async () => {
+    await signOut({ redirect: false });
+    await router.push('/');
+    toast.success('Logged out');
     resetStore();
-    await signOut({ callbackUrl: '/' }).then(() => {
-      // TODO: Make it a toast
-      console.log('Successfully signed out');
-    });
-  }, [resetStore]);
+  }, [resetStore, router]);
 
   const leaveWorkspace = useCallback(() => {
     resetStore();
@@ -40,10 +43,12 @@ export function AvatarDrop() {
       : void signIn(undefined, { callbackUrl: '/workboard' }),
   );
 
+  if (!sessionData) return null;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" title="user info">
+        <Button variant="outline" size="icon" title="user info">
           <Avatar className="h-6 w-6">
             <AvatarImage
               src="https://source.boringavatars.com/bauhaus"
@@ -54,54 +59,46 @@ export function AvatarDrop() {
           <span className="sr-only">User info</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-64" align="end" forceMount>
-        {!sessionData ? (
+      <DropdownMenuContent className="w-fit" align="end" forceMount>
+        <DropdownMenuItem>
+          <Icons.user className="mr-2 h-4 w-4" />
+          <div className="flex flex-col gap-2">
+            <p className="text-sm font-medium leading-none">
+              {sessionData.user?.name}
+            </p>
+            <p className="text-xs leading-none text-muted-foreground dark:text-gray-500">
+              {sessionData.user?.email}
+            </p>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <Link href="/">
+            <DropdownMenuItem className="cursor-pointer">
+              <ArrowLeftIcon className="mr-2 h-4 w-4" />
+              <span>Home</span>
+            </DropdownMenuItem>
+          </Link>
+
           <DropdownMenuItem
-            onClick={() =>
-              void signIn(undefined, { callbackUrl: '/workboard' })
-            }
+            className="cursor-pointer"
+            onClick={() => leaveWorkspace()}
           >
-            <Icons.login className="mr-2 h-4 w-4" />
-            <span>Sign In</span>
-            <DropdownMenuShortcut>Shift+Alt+L</DropdownMenuShortcut>
+            <Icons.folderx className="mr-2 h-4 w-4" />
+            <span>
+              Leave{' '}
+              <span className="text-purple-500 dark:text-purple-400 font-semibold">
+                workspace
+              </span>
+            </span>
           </DropdownMenuItem>
-        ) : (
-          <>
-            <DropdownMenuItem>
-              <Icons.user className="mr-2 h-4 w-4" />
-              <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  {sessionData.user?.name}
-                </p>
-                <p className="text-xs leading-none text-muted-foreground dark:text-gray-500">
-                  {sessionData.user?.email}
-                </p>
-              </div>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              {!!workspacePath && (
-                <DropdownMenuItem onClick={() => leaveWorkspace()}>
-                  <>
-                    <Icons.folderx className="mr-2 h-4 w-4" />
-                    <span>
-                      Leave{' '}
-                      <span className="text-purple-500 dark:text-purple-400 font-semibold">
-                        workspace
-                      </span>
-                    </span>
-                  </>
-                </DropdownMenuItem>
-              )}
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={() => void logOut()}>
-              <Icons.logout className="mr-2 h-4 w-4" />
-              <span>Sign Out</span>
-              <DropdownMenuShortcut>Shift+Alt+L</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </>
-        )}
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => void logOut()}>
+          <Icons.logout className="mr-2 h-4 w-4" />
+          <span>Sign Out</span>
+          <DropdownMenuShortcut>Shift+Alt+L</DropdownMenuShortcut>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from 'next-themes';
 import * as React from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
+import { ControlButton } from 'reactflow';
 import { Icons } from '~/components/icons';
 import { Button } from '~/components/ui/button';
 
@@ -21,5 +22,24 @@ export function ThemeToggle() {
       <Icons.moon className="absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
+  );
+}
+
+export function ControlThemeButton() {
+  const { theme, setTheme } = useTheme();
+  useHotkeys('shift+alt+d', () =>
+    setTheme(theme === 'light' ? 'dark' : 'light'),
+  );
+
+  return (
+    <ControlButton
+      title="toggle theme"
+      className="[&>svg]:!max-w-[24px] [&>svg]:!max-h-6"
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+    >
+      <Icons.sun className="h-4 w-4 rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100" />
+      <Icons.moon className="h-4 w-4 absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100" />
+      <span className="sr-only">Toggle theme</span>
+    </ControlButton>
   );
 }

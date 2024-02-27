312897aaa2c01d41648f1d709f52e606abefdab7
Bruno Carlos <bruno.carlos@lnls.br>
2024-02-27 12:39:56 -0300

    New home page with logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,33 @@
 import { ArrowRightIcon } from 'lucide-react';
 import { type NextPage } from 'next';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import { AvatarDrop } from '~/components/avatar-dropdown';
 import { Layout } from '~/components/layout';
 
 const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
+
+  if (!sessionData)
+    return (
+      <button
+        className=" w-3/4 rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={() => void signIn(undefined, { callbackUrl: '/workboard' })}
+      >
+        {'Sign in'}
+      </button>
+    );
+
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-      </p>
-      <div className="flex gap-4">
-        <button
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-          onClick={
-            sessionData
-              ? () => void signOut()
-              : () => void signIn(undefined, { callbackUrl: '/workboard' })
-          }
-        >
-          {sessionData ? 'Sign out' : 'Sign in'}
-        </button>
-        {sessionData && (
-          <Link
-            href="/workboard"
-            className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-          >
-            <span>Workboard</span>
-            <ArrowRightIcon className="ml-2 inline-block h-5 w-5 text-white" />
-          </Link>
-        )}
-      </div>
-    </div>
+    <Link
+      href="/workboard"
+      className="w-3/4 text-center rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+    >
+      <span>Workboard</span>
+      <ArrowRightIcon className="ml-2 inline-block h-5 w-5 text-white" />
+    </Link>
   );
 };
 
@@ -49,17 +42,25 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/icon.svg" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#5363e0c7] to-[#211731]">
-        <div className="container flex flex-row items-center justify-center gap-12 px-4 py-16 ">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#757cb3c7] via-[#5046bec7] to-[#2a1d40] dark:bg-gradient-to-b dark:from-[#2a1d40] dark:via-[#5046bec7] dark:to-[#757cb3c7]">
+        <div className="absolute top-5 right-5 ">
+          <AvatarDrop />
+        </div>
+        <div className="container flex lg:flex-row flex-col items-center justify-center lg:gap-12 px-4 py-16 ">
           <Image
             src="/full-transp.svg"
             alt="DeepSirius Logo"
             width={500}
             height={500}
           />
-          <div className="flex flex-col gap-4">
-            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-              <span className="text-[hsl(280,100%,70%)]">Deep</span>Sirius
+          <div className="flex flex-col items-center gap-12">
+            <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem] text-center">
+              <span className="text-[hsl(0,0%,0%)] dark:text-slate-200">
+                Deep
+              </span>
+              <span className="text-slate-200 dark:text-[hsl(0,0%,0%)]">
+                Sirius
+              </span>
             </h1>
             <AuthShowcase />
           </div>

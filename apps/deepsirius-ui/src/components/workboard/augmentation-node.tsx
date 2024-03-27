aa632e74bdbdf193dcb65325aa02eb4de51908c9
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-03-27 18:49:49 -0300

    feat: new augmentation node (needs new processing container).

---
+++ apps/deepsirius-ui/src/components/workboard/augmentation-node.tsx
@@ -0,0 +1,300 @@
+import {
+  type FormType,
+  AugmentationForm,
+} from './node-component-forms/augmentation-form';
+
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+import { type NodeProps, useUpdateNodeInternals } from 'reactflow';
+import { toast } from 'sonner';
+import { type NodeData, useStoreActions } from '~/hooks/use-store';
+import { api } from '~/utils/api';
+import NodeCard from './node-components/node-card';
+import {
+  ActiveSheet,
+  BusySheet,
+  ErrorSheet,
+  SuccessSheet,
+} from './node-components/node-sheet';
+
+export function AugmentationNode(nodeProps: NodeProps<NodeData>) {
+  const [open, setOpen] = useState(false);
+
+  const { onUpdateNode, getSourceData } = useStoreActions();
+
+  const [formData, setFormData] = useState<FormType | undefined>(
+    nodeProps.data.form as FormType,
+  );
+
+  const getSourceDatasetName = () => {
+    const sourceNode = getSourceData(nodeProps.id);
+    if (sourceNode?.remotePath) {
+      try {
+        return (
+          sourceNode.remotePath.split('/')?.pop()?.replace('.h5', '') || ''
+        );
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    return '';
+  };
+
+  const getSuggestedName = () => {
+    const name = getSourceDatasetName();
+    return name ? name + '_augmented' : '';
+  };
+
+  useEffect(() => {
+    const formEditState =
+      nodeProps.selected &&
+      ['active', 'error', 'success'].includes(nodeProps.data.status);
+    setOpen(formEditState);
+  }, [nodeProps.selected, nodeProps.data.status]);
+
+  const updateNodeInternals = useUpdateNodeInternals();
+
+  const { data: jobData } = api.job.checkStatus.useQuery(
+    { jobId: nodeProps.data.jobId as string },
+    {
+      refetchOnMount: false,
+      enabled: nodeProps.data.status === 'busy' && !!nodeProps.data.jobId,
+      refetchInterval: 5000,
+      refetchIntervalInBackground: true,
+    },
+  );
+
+  useEffect(() => {
+    if (!jobData) return;
+    if (jobData.jobStatus === 'COMPLETED') {
+      const date = dayjs().format('YYYY-MM-DD HH:mm:ss');
+      console.log('Job completed');
+      toast.success('Job completed');
+      onUpdateNode({
+        id: nodeProps.id,
+        data: {
+          ...nodeProps.data,
+          status: 'success',
+          jobId: nodeProps.data.jobId,
+          jobStatus: jobData.jobStatus,
+          message: `Job ${
+            nodeProps.data.jobId ?? 'aa'
+          } finished successfully in ${date}`,
+          updatedAt: date,
+        },
+      });
+      updateNodeInternals(nodeProps.id);
+    } else if (jobData.jobStatus === 'FAILED') {
+      const date = dayjs().format('YYYY-MM-DD HH:mm:ss');
+      console.log('Job failed');
+      toast.error('Job failed');
+      onUpdateNode({
+        id: nodeProps.id,
+        data: {
+          ...nodeProps.data,
+          status: 'error',
+          jobId: nodeProps.data.jobId,
+          jobStatus: jobData.jobStatus,
+          message: `Job ${nodeProps.data.jobId ?? 'aa'} failed in ${date}`,
+          updatedAt: date,
+        },
+      });
+      updateNodeInternals(nodeProps.id);
+    } else {
+      const date = dayjs().format('YYYY-MM-DD HH:mm:ss');
+      console.log('Job is busy');
+      onUpdateNode({
+        id: nodeProps.id,
+        data: {
+          ...nodeProps.data,
+          status: 'busy',
+          jobId: nodeProps.data.jobId,
+          jobStatus: jobData.jobStatus,
+          message: `Job ${nodeProps.data.jobId ?? 'aa'} is ${
+            jobData.jobStatus?.toLocaleLowerCase() ?? 'aa'
+          }, last checked at ${date}`,
+          updatedAt: date,
+        },
+      });
+      updateNodeInternals(nodeProps.id);
+    }
+  }, [
+    jobData,
+    nodeProps.data,
+    nodeProps.id,
+    onUpdateNode,
+    updateNodeInternals,
+  ]);
+
+  const { mutateAsync: cancelJob } = api.job.cancel.useMutation();
+  const { mutateAsync: submitJob } =
+    api.deepsiriusJob.submitAugmentation.useMutation();
+
+  const handleSubmit = (formData: FormType) => {
+    console.log('handleSubmitJob');
+    const name = getSourceDatasetName();
+    if (!name) {
+      toast.warning('Please connect a dataset');
+      return;
+    }
+    console.log('will submit', formData, name);
+    submitJob({
+      formData: formData,
+      datasetName: name,
+      workspacePath: nodeProps.data.workspacePath,
+    })
+      .then(({ jobId }) => {
+        onUpdateNode({
+          id: nodeProps.id,
+          data: {
+            ...nodeProps.data,
+            status: 'busy',
+            remotePath: `${nodeProps.data.workspacePath}/datasets/${formData.augmentedDatasetName}.h5`, // this is not ideal => we shouldnd guess the path
+            jobId: jobId,
+            message: `Job ${jobId} submitted in ${dayjs().format(
+              'YYYY-MM-DD HH:mm:ss',
+            )}`,
+            form: formData,
+          },
+        });
+        updateNodeInternals(nodeProps.id);
+        setFormData(formData);
+        setOpen(!open);
+        toast.success('Job submitted');
+      })
+      .catch((e) => {
+        toast.error('Error submitting job');
+        console.error(e);
+      });
+  };
+
+  const handleCancel = () => {
+    cancelJob({ jobId: nodeProps.data.jobId as string })
+      .then(() => {
+        const date = dayjs().format('YYYY-MM-DD HH:mm:ss');
+        toast.success('Job canceled');
+        onUpdateNode({
+          id: nodeProps.id,
+          data: {
+            ...nodeProps.data,
+            status: 'error',
+            jobId: nodeProps.data.jobId,
+            jobStatus: 'CANCELLED',
+            message: `Job ${nodeProps.data.jobId ?? 'aa'} canceled in ${date}`,
+            updatedAt: date,
+          },
+        });
+        updateNodeInternals(nodeProps.id);
+      })
+      .catch(() => {
+        toast.error('Error canceling job');
+      });
+    toast.info('Canceling job..');
+  };
+
+  if (nodeProps.data.status === 'active') {
+    return (
+      <>
+        <NodeCard
+          nodeType={nodeProps.type}
+          selected={nodeProps.selected}
+          title={'augmentation'}
+          subtitle={'Click to create an augmented dataset'}
+          nodeStatus={nodeProps.data.status}
+        />
+        <ActiveSheet
+          selected={nodeProps.selected}
+          title={'Dataset Augmentation'}
+        >
+          <AugmentationForm
+            onSubmitHandler={handleSubmit}
+            name={getSuggestedName()}
+          />
+        </ActiveSheet>
+      </>
+    );
+  }
+
+  if (nodeProps.data.status === 'busy') {
+    return (
+      <>
+        <NodeCard
+          nodeType={nodeProps.type}
+          selected={nodeProps.selected}
+          title={'augmentation'}
+          subtitle={'Busy'}
+          nodeStatus={nodeProps.data.status}
+        />
+        <BusySheet
+          selected={nodeProps.selected}
+          title={'Details'}
+          jobId={nodeProps.data.jobId}
+          jobStatus={nodeProps.data.jobStatus}
+          updatedAt={nodeProps.data.updatedAt}
+          message={nodeProps.data.message}
+          handleCancel={handleCancel}
+        />
+      </>
+    );
+  }
+
+  if (nodeProps.data.status === 'success') {
+    return (
+      <>
+        <NodeCard
+          nodeType={nodeProps.type}
+          selected={nodeProps.selected}
+          title={'augmentation'}
+          subtitle={'Success'}
+          nodeStatus={nodeProps.data.status}
+        />
+        <SuccessSheet
+          selected={nodeProps.selected}
+          message={nodeProps.data.message}
+        >
+          <div className="flex flex-col gap-2 rounded-md border border-input p-2 font-mono">
+            {formData &&
+              Object.entries(formData)
+                .filter(([_, value]) => typeof value === 'string')
+                .map(([key, value]) => (
+                  <div
+                    key={key}
+                    className="flex flex-row items-center justify-between gap-1"
+                  >
+                    <p className="font-medium">{key}</p>
+                    <p className="text-end text-violet-600">
+                      {value as string}
+                    </p>
+                  </div>
+                ))}
+          </div>
+        </SuccessSheet>
+      </>
+    );
+  }
+
+  if (nodeProps.data.status === 'error') {
+    return (
+      <>
+        <NodeCard
+          nodeType={nodeProps.type}
+          selected={nodeProps.selected}
+          title={'augmentation'}
+          subtitle={'Error'}
+          nodeStatus={nodeProps.data.status}
+        />
+        <ErrorSheet
+          selected={nodeProps.selected}
+          message={nodeProps.data.message}
+        >
+          <AugmentationForm
+            onSubmitHandler={handleSubmit}
+            name={formData?.augmentedDatasetName || ''}
+          />
+        </ErrorSheet>
+      </>
+    );
+  }
+
+  return null;
+}
---
+++ apps/deepsirius-ui/src/components/workboard/node-component-forms/augmentation-form.tsx
@@ -0,0 +1,375 @@
+'use client';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { slurmPartitionOptions } from '~/lib/constants';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '~/components/ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/components/ui/select';
+import { Input } from '~/components/ui/input';
+import { Button } from '~/components/ui/button';
+import { Switch } from '~/components/ui/switch';
+
+const slurmOptions = z.object({
+  partition: z.enum(slurmPartitionOptions),
+});
+
+export const augmentationSchema = z.object({
+  slurmOptions,
+  augmentedDatasetName: z
+    .string()
+    .min(1, { message: 'Must have a name!' })
+    .refine((s) => !s.includes(' '), 'No Spaces!'),
+  vflip: z.boolean(),
+  hflip: z.boolean(),
+  rotateClock: z.boolean(),
+  rotateCClock: z.boolean(),
+  contrast: z.boolean(),
+  dropout: z.boolean(),
+  linearContrast: z.boolean(),
+  gaussianBlur: z.boolean(),
+  poissonNoise: z.boolean(),
+  averageBlur: z.boolean(),
+  elasticDeformation: z.boolean(),
+});
+
+export type FormType = z.input<typeof augmentationSchema>;
+export type FormCallback = (data: FormType) => void;
+
+export type FormProps = {
+  onSubmitHandler: FormCallback;
+  name?: FormType['augmentedDatasetName'];
+};
+
+function useAugmentationForm(name: FormProps['name'] = '') {
+  const form = useForm<FormType>({
+    resolver: zodResolver(augmentationSchema),
+    defaultValues: {
+      augmentedDatasetName: name,
+      vflip: false,
+      hflip: false,
+      rotateClock: false,
+      rotateCClock: false,
+      contrast: false,
+      dropout: false,
+      linearContrast: false,
+      gaussianBlur: false,
+      poissonNoise: false,
+      averageBlur: false,
+      elasticDeformation: false,
+    },
+  });
+
+  return form;
+}
+
+export function AugmentationForm({ onSubmitHandler, name }: FormProps) {
+  const form = useAugmentationForm(name);
+  const onSubmit = () => {
+    onSubmitHandler(form.getValues());
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        className="flex flex-col gap-4"
+        onSubmit={(...args) => void form.handleSubmit(onSubmit)(...args)}
+      >
+        <FormField
+          control={form.control}
+          name="augmentedDatasetName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Dataset Name</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="dataset++" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="grid grid-cols-2 gap-x-4 rounded-md border border-dashed px-4">
+          <FormField
+            name="vflip"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Vertical Flip
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="hflip"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Horizontal Flip
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="rotateCClock"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Rotate 90
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="rotateClock"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Rotate -90
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="contrast"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Contrast
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="linearContrast"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Linear Contrast
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="dropout"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Dropout
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            name="gaussianBlur"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Gaussian Blur
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            name="poissonNoise"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Poisson Noise
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="averageBlur"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Average Blur
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="elasticDeformation"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="">
+                <FormControl>
+                  <div className=" flex items-center space-x-4 py-4">
+                    <div className="flex-1 space-y-1">
+                      <p className="text-sm font-medium leading-none">
+                        Elastic Deformation
+                      </p>
+                    </div>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <footer className="flex flex-row gap-2">
+          <FormField
+            control={form.control}
+            name="slurmOptions.partition"
+            render={({ field }) => (
+              <FormItem className="w-1/2">
+                <FormLabel className="sr-only">Slurm Partition</FormLabel>
+                <Select onValueChange={field.onChange}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Slurm Partition" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {slurmPartitionOptions.map((item) => (
+                      <SelectItem key={item} value={item}>
+                        {item}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormDescription className="hidden">
+                  Please select a slurm partition assigned for your user for
+                  submitting this job.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="mt-2 w-1/2" type="submit">
+            Submit Job
+          </Button>
+        </footer>
+      </form>
+    </Form>
+  );
+}

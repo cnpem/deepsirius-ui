03e397844844cbda981f00303c187f191a664e5f
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-05-21 18:03:52 -0300

    fix: checking for pending status needs another command, since the job is not submitted yet.

--- apps/deepsirius-ui/src/server/api/routers/job.ts
+++ apps/deepsirius-ui/src/server/api/routers/job.ts
@@ -26,6 +26,20 @@ export const jobRouter = createTRPCRouter({
       // The output of the sacct command comes in two lines, the first line is the header and the second is the actual state: i.e. State\nRUNNING, State\nCOMPLETED, etc.
       const lines = stdout.trim().split('\n');
       const status = lines[1];
+      if (!status) {
+        // If the status is empty, it means the job.batch wasn't found, but the job might be PENDING
+        const command = `sacct -j ${jobId} --format=State --parsable2`;
+        const { stdout, stderr } = await connection.execCommand(command);
+        if (stderr) {
+          throw new TRPCError({
+            code: 'INTERNAL_SERVER_ERROR',
+            message: stderr,
+          });
+        }
+        const lines = stdout.trim().split('\n');
+        const status = lines[1];
+        return { jobStatus: status };
+      }
       return { jobStatus: status };
     }),
   cancel: protectedSSHProcedure

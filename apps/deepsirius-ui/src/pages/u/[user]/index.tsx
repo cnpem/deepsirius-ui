683d36fa90ecf76d78d9279368644d5fc4119910
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-05-24 17:19:58 -0300

    fix: fixing loading states appearence and logic with better separation of concerns on components of the user page.

--- apps/deepsirius-ui/src/pages/u/[user]/index.tsx
+++ apps/deepsirius-ui/src/pages/u/[user]/index.tsx
@@ -18,8 +18,9 @@ import { Input } from '~/components/ui/input';
 import { Skeleton } from '~/components/ui/skeleton';
 import { useUser } from '~/hooks/use-user';
 import { api } from '~/utils/api';
+import { type WorkspaceState } from '@prisma/client';
 
-const User: NextPage = () => {
+const UserPage: NextPage = () => {
   const user = useUser();
   const router = useRouter();
 
@@ -34,58 +35,18 @@ const User: NextPage = () => {
 
   return (
     <LayoutNav>
-      <UserWorkspaces />
+      <UserWorkspaces userRoute={user.route} />
     </LayoutNav>
   );
 };
 
-export default User;
+type WorkspaceSelectProps = {
+  path: string;
+  state: string;
+};
 
-function UserWorkspaces() {
-  const user = useUser();
-  const { data: userWorkspaces, isLoading } =
-    api.db.getUserWorkspaces.useQuery();
-  const utils = api.useUtils();
-
+function UserWorkspaces({ userRoute }: { userRoute: string }) {
   const router = useRouter();
-
-  type WorkspaceSelectProps = {
-    path: string;
-    state: string;
-  };
-
-  const handleSelectWorkspace = useCallback(
-    async (props: WorkspaceSelectProps) => {
-      if (!user) return;
-      await router.push(user.route + '/' + workspaceShortName(props.path));
-    },
-    [router, user],
-  );
-
-  const { mutate: deleteWorkspace } = api.ssh.rmWorkspace.useMutation({
-    onSuccess: async () => {
-      await utils.db.getUserWorkspaces.invalidate();
-      toast.success('Workspace deleted');
-    },
-    onError: () => {
-      toast.error('Error deleting workspace');
-    },
-  });
-
-  const { mutate: updateFavoriteWorkspace } =
-    api.db.updateFavoriteWorkspace.useMutation({
-      onSuccess: async () => {
-        await utils.db.getUserWorkspaces.invalidate();
-        toast.success('Favorite Updated');
-      },
-      onError: () => {
-        toast.error('Error updating favorite workspace');
-      },
-    });
-
-  const workspaceShortName = (path: string) => {
-    return path.split('/').pop() as string;
-  };
 
   const handleNewWorkspace = async () => {
     await router.push('/new');
@@ -97,95 +58,26 @@ function UserWorkspaces() {
     setSearch(e.target.value);
   }, []);
 
-  const searchAndSortWorkspaces = userWorkspaces
-    ?.filter(({ path }) => path.includes(search))
-    .sort((a, b) => {
-      return a.favorite === b.favorite ? 0 : a.favorite ? -1 : 1;
-    });
-
-  if (isLoading) {
-    return <Skeleton />;
-  }
-
-  if (userWorkspaces) {
-    return (
-      <div className="mx-auto flex w-full flex-col gap-2 px-4 md:w-1/2">
-        <div className="flex flex-row gap-4 pb-4 pt-0">
-          <Input
-            className="border-none bg-slate-300 bg-opacity-10 text-slate-200 placeholder:text-slate-300"
-            type="text"
-            placeholder="Search workspaces"
-            onChange={handleSearch}
-          />
-          <button
-            className="inline-flex items-center rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={() => void handleNewWorkspace()}
-          >
-            <PlusSquareIcon className="mr-2 h-4 w-4" />
-            {'New'}
-          </button>
-        </div>
-
-        {!searchAndSortWorkspaces && (
-          <div className="flex flex-col gap-4">
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-          </div>
-        )}
-
-        {searchAndSortWorkspaces?.map((workspace) => (
-          <Card
-            key={workspace.path}
-            className="border border-blue-400 bg-white text-blue-800 dark:border-blue-600 dark:bg-slate-900 dark:text-blue-500"
-          >
-            <div className="grid grid-cols-2 justify-between gap-4 py-4 pl-8 pr-4">
-              <div className="mr-auto">
-                <CardHeader className="align-start p-0 text-start">
-                  <CardTitle>
-                    <Button
-                      className="p-0 text-lg font-semibold text-blue-800 dark:text-blue-500"
-                      variant="link"
-                      onClick={() =>
-                        void handleSelectWorkspace({
-                          path: workspace.path,
-                          state: workspace.state,
-                        })
-                      }
-                    >
-                      {workspaceShortName(workspace.path)}
-                    </Button>
-                  </CardTitle>
-                  <CardDescription>{workspace.path}</CardDescription>
-                </CardHeader>
-                <CardFooter className="my-2 p-0 text-sm text-muted-foreground">
-                  <p>
-                    {'Last updated at: ' +
-                      new Date(workspace.updatedAt).toLocaleString()}
-                  </p>
-                </CardFooter>
-              </div>
-              <div className="ml-auto">
-                <WorkspaceCardActions
-                  path={workspace.path}
-                  favorite={workspace.favorite}
-                  handleDelete={deleteWorkspace}
-                  handleFavorite={({ path }: { path: string }) =>
-                    updateFavoriteWorkspace({
-                      path,
-                      favorite: !workspace.favorite,
-                    })
-                  }
-                />
-              </div>
-            </div>
-          </Card>
-        ))}
+  return (
+    <div className="mx-auto flex w-full flex-col gap-2 px-4 md:w-1/2">
+      <div className="flex flex-row gap-4 pb-4 pt-0">
+        <Input
+          className="border-none bg-slate-300 bg-opacity-10 text-slate-200 placeholder:text-slate-300"
+          type="text"
+          placeholder="Search workspaces"
+          onChange={handleSearch}
+        />
+        <button
+          className="inline-flex items-center rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={() => void handleNewWorkspace()}
+        >
+          <PlusSquareIcon className="mr-2 h-4 w-4" />
+          {'New'}
+        </button>
       </div>
-    );
-  }
-  // if we get here, theres a problem with the db
-  return <p>Something went wrong</p>;
+      <WorkspaceList userRoute={userRoute} search={search} />
+    </div>
+  );
 }
 
 interface WorkspaceCardActionsProps {
@@ -194,12 +86,12 @@ interface WorkspaceCardActionsProps {
   handleDelete: ({ path }: { path: string }) => void;
   handleFavorite: ({ path }: { path: string }) => void;
 }
-const WorkspaceCardActions = ({
+function WorkspaceCardActions({
   path,
   favorite,
   handleDelete,
   handleFavorite,
-}: WorkspaceCardActionsProps) => {
+}: WorkspaceCardActionsProps) {
   const [alertOpen, setAlertOpen] = useState(false);
   return (
     <>
@@ -229,4 +121,140 @@ const WorkspaceCardActions = ({
       </div>
     </>
   );
+}
+
+type WorkspaceListProps = {
+  userRoute: string;
+  search: string;
 };
+
+function WorkspaceList({ userRoute, search }: WorkspaceListProps) {
+  const utils = api.useUtils();
+  const router = useRouter();
+
+  function workspaceShortName(path: string) {
+    return path.split('/').pop() as string;
+  }
+
+  function sortAndSearchWorkspaces({
+    workspaces,
+    search,
+  }: {
+    workspaces: WorkspaceState[];
+    search: string;
+  }) {
+    if (!workspaces) return [];
+    const sorted = workspaces.sort((a, b) => {
+      return a.favorite === b.favorite ? 0 : a.favorite ? -1 : 1;
+    });
+    if (!search) return sorted;
+    return sorted.filter((workspace) =>
+      workspace.path.toLowerCase().includes(search.toLowerCase()),
+    );
+  }
+
+  const { data: workspaces, isLoading } = api.db.getUserWorkspaces.useQuery();
+
+  const handleSelect = useCallback(
+    async (props: WorkspaceSelectProps) => {
+      await router.push(userRoute + '/' + workspaceShortName(props.path));
+    },
+    [router, userRoute],
+  );
+
+  const { mutate: deleteWorkspace } = api.ssh.rmWorkspace.useMutation({
+    onSuccess: async () => {
+      await utils.db.getUserWorkspaces.invalidate();
+      toast.success('Workspace deleted');
+    },
+    onError: () => {
+      toast.error('Error deleting workspace');
+    },
+  });
+
+  const { mutate: updateFavorite } = api.db.updateFavoriteWorkspace.useMutation(
+    {
+      onSuccess: async () => {
+        await utils.db.getUserWorkspaces.invalidate();
+        toast.success('Favorite Updated');
+      },
+      onError: () => {
+        toast.error('Error updating favorite workspace');
+      },
+    },
+  );
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col gap-2">
+        <Skeleton className="flex flex-row h-36 w-full justify-center"/>
+        <Skeleton className="flex flex-row h-36 w-full justify-center"/>
+        <Skeleton className="flex flex-row h-36 w-full justify-center"/>
+      </div>
+    );
+  }
+
+  if (!workspaces || workspaces.length === 0) {
+    return (
+      <div className="flex flex-col">
+        <Skeleton className="flex flex-row h-36 w-full justify-center">
+          <p className='my-auto'>No workspaces found</p>
+        </Skeleton>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      {sortAndSearchWorkspaces({ workspaces, search }).map((workspace) => (
+        <Card
+          key={workspace.path}
+          className="border border-blue-400 bg-white text-blue-800 dark:border-blue-600 dark:bg-slate-900 dark:text-blue-500"
+        >
+          <div className="grid grid-cols-2 justify-between gap-4 py-4 pl-8 pr-4">
+            <div className="mr-auto">
+              <CardHeader className="align-start p-0 text-start">
+                <CardTitle>
+                  <Button
+                    className="p-0 text-lg font-semibold text-blue-800 dark:text-blue-500"
+                    variant="link"
+                    onClick={() =>
+                      void handleSelect({
+                        path: workspace.path,
+                        state: workspace.state,
+                      })
+                    }
+                  >
+                    {workspaceShortName(workspace.path)}
+                  </Button>
+                </CardTitle>
+                <CardDescription>{workspace.path}</CardDescription>
+              </CardHeader>
+              <CardFooter className="my-2 p-0 text-sm text-muted-foreground">
+                <p>
+                  {'Last updated at: ' +
+                    new Date(workspace.updatedAt).toLocaleString()}
+                </p>
+              </CardFooter>
+            </div>
+            <div className="ml-auto">
+              <WorkspaceCardActions
+                path={workspace.path}
+                favorite={workspace.favorite}
+                handleDelete={deleteWorkspace}
+                handleFavorite={({ path }: { path: string }) =>
+                  updateFavorite({
+                    path,
+                    favorite: !workspace.favorite,
+                  })
+                }
+              />
+            </div>
+          </div>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default UserPage;

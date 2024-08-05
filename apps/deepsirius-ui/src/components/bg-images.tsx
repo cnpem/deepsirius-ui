27c9e2d595788949ae4d1b92154e58b5627ff049
Matheus Luís <matheus.bernardi@lnls.br>
2024-08-05 16:04:11 -0300

    use public img url directly in bg images

--- apps/deepsirius-ui/src/components/bg-images.tsx
+++ apps/deepsirius-ui/src/components/bg-images.tsx
@@ -1,30 +1,13 @@
-import Image, { type StaticImageData } from 'next/image';
-import topImg from '@public/top.svg';
-import bottomImg from '@public/bottom.svg';
-import fullImg from '@public/full.svg';
+import Image from 'next/image';
 
 const TopBgImage = () => (
-  <Image
-    src={topImg as StaticImageData}
-    alt={''}
-    fill
-    priority
-    className="py-4"
-  />
+  <Image src={'/top.svg'} alt={''} fill priority className="py-4" />
 );
 
 const BottomBgImage = () => (
-  <Image
-    src={bottomImg as StaticImageData}
-    alt={''}
-    fill
-    priority
-    className="py-4"
-  />
+  <Image src={'/bottom.svg'} alt={''} fill priority className="py-4" />
 );
 
-const FullBgImage = () => (
-  <Image src={fullImg as StaticImageData} alt={''} fill priority />
-);
+const FullBgImage = () => <Image src={'/full.svg'} alt={''} fill priority />;
 
 export { TopBgImage, BottomBgImage, FullBgImage };
